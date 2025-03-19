cbcbf59b92bd2257c368955c1fb8942ed5def01a
jakebentley2001 <jakebentley2001@gmail.com>
2025-03-19 16:28:07 -0400

    fixed data path

--- training_code/jake-finetuning-trial.py
+++ training_code/jake-finetuning-trial.py
@@ -35,9 +35,9 @@ from model_tools import keep_single_char_tokens, save_model_and_tokenizer
 
 # input paths
 base_model = 'nvidia/Mistral-NeMo-Minitron-8B-Base'  # auto-downloaded from huggingface.co
-arc_data_path = os.path.join('input', 'arc-prize-2024')  # as on kaggle arc prize 2024
-re_arc_path = os.path.join('input', 're_arc')  # https://github.com/michaelhodel/re-arc
-neoneye_path = os.path.join('input', 'arc-dataset-collection')  # https://github.com/neoneye/arc-dataset-collection
+arc_data_path = os.path.join('input/arc-data/ARC-DATA/input', 'arc-prize-2024')  # as on kaggle arc prize 2024
+re_arc_path = os.path.join('input/arc-data/ARC-DATA/input', 're_arc')  # https://github.com/michaelhodel/re-arc
+neoneye_path = os.path.join('input/arc-data/ARC-DATA/input', 'arc-dataset-collection')  # https://github.com/neoneye/arc-dataset-collection
 
 # output paths
 save_model_path = os.path.join('pretrained_models', "Mistral-NeMo-Minitron-Full")
