16a2db7ad987649371e5cd3d5a47af3f3506434b
jakebentley2001 <jakebentley2001@gmail.com>
2025-03-24 16:50:15 -0400

    n = 184

--- training_code/multi-gpu-mistral.py
+++ training_code/multi-gpu-mistral.py
@@ -241,7 +241,7 @@ for action in ['train', 'merge']:
 
     if action == 'train':
         print("=== Starting TRAINING phase ===")
-        train_dataset = ArcDataset.load_from_rearc(re_arc_path, n=32, sizes=[6], seed=42)
+        train_dataset = ArcDataset.load_from_rearc(re_arc_path, n=184, sizes=[6], seed=42)
        # train_dataset = ArcDataset.load_from_rearc(re_arc_path, n=368, sizes=[6], seed=42)
 
         train_aug_opts = dict(tp=True, rt=True, perm=True, shfl_ex=True, seed=0)
